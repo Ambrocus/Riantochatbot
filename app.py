ff558fb8e50717569996d5b6190d6d79150c9996
Ambrocus <nicholas.rianto@gmail.com>
2025-02-19 19:15:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app.config['API_KEY'] = os.getenv('API_KEY')
 from chatbot import respond
 @app.route('/')
 def home():
-    return 
-render_template('index.html')
+    with app.app_context():
+        return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
